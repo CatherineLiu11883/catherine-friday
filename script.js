6a85a29e95d2d4e36db2306c0fc5d1178c2190fc
CatherineLiu11883 <catherinechangliu@gmail.com>
2016-10-21 21:44:26 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 var elementContainer = $("#generated-element-container")
 
-$("#make-blue-element").click(function() {  
-  var newElement = $("<div>I guess green is better!</div>")
-  newElement.css({background: "blue"})
-}
+//$("#make-blue-element").click(function() {  
+//  var newElement = $("<div>I guess green is better!</div>")
+//  newElement.css({background: "blue"})
+//  elementContainer.append(newElement)
+//})
   
-  $("#make-green-element").click(function() {  
+$("#make-green-element").click(function() {  
     var newElement = $("<div>I guess green is better!</div>")
-    newElement.css({background: "blue"})
-}
+    newElement.css({background: "blue"})      elementContainer.append(newElement)
+})
     
-    $("#make-orange-element").click(function() {  
-      var newElement = $("<div>I guess green is better!</div>")
-      newElement.css({background: "blue"})
-}
-  
+//    $("#make-orange-element").click(function() {  
+//      var newElement = $("<div>I guess green is better!</div>")
+//      newElement.css({background: "blue"})
+//      elementContainer.append(newElement)
+//})
+//  
  
