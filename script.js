fa0fbac4fe7e37a169a4379a6ef2db2bf2b84dff
CatherineLiu11883 <catherinechangliu@gmail.com>
2016-10-21 21:49:35 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ var elementContainer = $("#generated-element-container")
   //newElement.css({background: "blue"})
 //  elementContainer.append(newElement)
 //})
-  
+
 $("#make-green-element").click(function() {  
     var newElement = $("<div>I love grass and trees!</div>")
-    newElement.css({background: "blue"})      elementContainer.append(newElement)
+    newElement.css({background: "blue"})
+    elementContainer.append(newElement)
 })
     
 //    $("#make-orange-element").click(function() {  
